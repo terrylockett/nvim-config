19b4757777154f2a1bd92b78c83adf56a9d414be
Terry Lockett <terryjlockett@gmail.com>
2024-02-14 10:27:20 -0400

    adding groovy lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = {"lua_ls", "gradle_ls"}
+				ensure_installed = {"lua_ls", "gradle_ls", "groovyls"}
 			})
 		end
 	},
@@ -19,11 +19,16 @@ return {
 			local lspconfig = require('lspconfig')
 			lspconfig.lua_ls.setup({})
 			lspconfig.gradle_ls.setup({})
+			lspconfig.groovyls.setup({
+				cmd = {"java", "-jar", "/Users/terry/.config/groovyls/groovy-language-server-all.jar" },
+				filetypes = {"groovy", "gradle"},
+			})
 
 			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+			vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
 			vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-
+			vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
 		end
 	}
 }
--- lua/terry/remap.lua
+++ lua/terry/remap.lua
@@ -1,2 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Global diagnostic Mappins
+-- vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float)
+-- vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+-- vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
